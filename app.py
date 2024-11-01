c8afd519f3e201fe40f65ca993186c8d1b375456
Damian Gillessen <damian.gillessen.2009@alumni.nuim.ie>
2024-11-01 14:42:28 -0600

    Add contact page route & run application

--- app.py
+++ app.py
@@ -112,4 +112,25 @@ def testimonials():
     
     return render_template('testimonials.html')
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Retrieve contact message data
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        
+        # Insert contact request into the database
+        conn = sqlite3.connect('db.sqlite3')
+        c = conn.cursor()
+        c.execute("INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)",
+                  (name, email, message))
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('contact.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
